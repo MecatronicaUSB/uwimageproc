ad318f9e828fa911e9d8b65d97c9ba05ed6b958f
Jose Cappelletto <cappelletto@gmail.com>
2017-03-18 11:21:52 -0400

    Implemented complete CLAHE estimation for CL/BS All functions performed in CPU. Missing curve fitting process yet

--- modules/aclahe/aclahe.cpp
+++ modules/aclahe/aclahe.cpp
@@ -14,7 +14,6 @@
 #include <iomanip>
 #include <sstream>
 #include <fstream>
-#include <stdlib.h>
 
 
 // OpenCV libraries
@@ -23,26 +22,39 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 #include <opencv2/video.hpp>
-#include <opencv2/features2d.hpp>
+/*#include <opencv2/features2d.hpp>
 #include "opencv2/calib3d.hpp"
 #include <opencv2/xfeatures2d.hpp>
-#include "opencv2/xfeatures2d/cuda.hpp"
+#include "opencv2/xfeatures2d/cuda.hpp"*/
 
 //CUDA libraries
 #include <opencv2/cudafilters.hpp>
-#include "opencv2/cudafeatures2d.hpp"
+//#include "opencv2/cudafeatures2d.hpp"
+
+//D-LIB Curve fitting
+#include <dlib/optimization.h>
 
 //#define _VERBOSE_ON_
+#define CL_MIN 0.0
+#define CL_MAX 25.0
+#define CL_STEP 0.25
 
+using namespace dlib;
 using namespace cv;
 using namespace cv::cuda;
-using namespace cv::xfeatures2d;
+//using namespace cv::xfeatures2d;
 using namespace std;
 
-char keyboard = 0;
 
+char keyboard = 0;
 // Timing monitor
 double t;
+// we create a 3-tuple structure to store BlockSize, ClipLimit and Entropy for each CLAHE image
+struct _entropy {
+    int BS;
+    float CL;
+    float Entropy;
+};
 
 //**** 1- Parse arguments from CLI
 //**** 2- Read input file
@@ -51,7 +63,7 @@ double t;
 //**** 5- Select breakpoint for Entropy vs CL
 //**** 6- Applies CL/BS to image
 
-float calcEntropy(Mat image);
+double calcEntropy(Mat image);
 
 int imgWidth, imgHeight;
 
@@ -87,6 +99,9 @@ int main(int argc, char *argv[]) {
     String OutputFile = cvParser.get<cv::String>(1);
     ostringstream OutputFileName;
 
+    ofstream outfile("out.txt", ios::out);
+
+
     if (! cvParser.check()) {
         cvParser.printErrors();
         return - 1;
@@ -110,8 +125,8 @@ int main(int argc, char *argv[]) {
 
     //**************************************************************************
     /* CUDA */
-    int nCuda = - 1;    //<Defines number of detected CUDA devices
 
+/*    int nCuda = - 1;    //<Defines number of detected CUDA devices
     nCuda = cuda::getCudaEnabledDeviceCount();
     cout << "Built with OpenCV " << CV_VERSION << endl;
     cuda::DeviceInfo deviceInfo;
@@ -124,17 +139,86 @@ int main(int argc, char *argv[]) {
     }
 
     cuda::setDevice(0);
+*/
+
+    //**************************************************************************
+    /* INPUT IMAGE */
+    //**************************************************************************
+
     cout << "***************************************" << endl;
     cout << "Input: " << InputFile << endl;
+
+    cout << "Reading input file" << endl;
+
+    Mat src = imread(InputFile, IMREAD_GRAYSCALE);
+    Mat dst = src.clone();
+    Mat tmp = dst.clone();
+
+    if (src.empty()) {
+        cout << "Error loading input file (empty resulting matrix" << endl;
+    }
+
+/*    imshow("Source image", src);
+    waitKey(100);*/
 
     //**************************************************************************
     /* GAUSSIAN FILTER */
     //**************************************************************************
+    GaussianBlur(src, dst, Size(3, 3), 0, 0);
+
+/*    imshow("Source image", dst);
+    waitKey(100);*/
+
+    //**************************************************************************
     /* CREATE CL AND BS VECTOR */
     //**************************************************************************
-    /* COMPUTE ENTROPY FOR EACH CASE */
+    static int m[] = {2, 4, 8, 16, 32};
+    vector<int> BlockSize(m, m + sizeof(m) / sizeof(m[0]));
+    vector<float> ClipLimit;
+
+    // Now, we must create a vector as in Matlab [0.0 : 0.25 : 25.0]
+    for (float f = CL_MIN; f <= CL_MAX; f += CL_STEP)
+        ClipLimit.push_back(f);
+
+    //**************************************************************************
+    /* APPLY CLAHE FOR EACH CL/BS COMBINATION */
+    //**************************************************************************
+    cv::Ptr<cv::CLAHE> clahe = cv::createCLAHE();
+    vector<_entropy> entropy;
+
+    // for speed purpose, we extract BSxBS subimages, and we operate over it
+    for (int j = 0; j < BlockSize.size(); j++) {
+        cout << "BS: " << BlockSize.at(j) << endl;
+        for (int i = 0; i < ClipLimit.size(); i++) {
+            //        cout << "CL: " << ClipLimit.at(i) << endl;
+            int BS = BlockSize.at(j);
+            clahe->setClipLimit(ClipLimit.at(i));
+            clahe->setTilesGridSize(Size(BS, BS));
+            clahe->apply(dst, tmp);
+            //**************************************************************************
+            /* COMPUTE ENTROPY FOR EACH CASE */
+            //**************************************************************************
+            //we store a copy of the current 3-tuple entropy data
+            _entropy curr_entropy;
+            curr_entropy.BS = BS;
+            curr_entropy.CL = ClipLimit.at(i);
+            curr_entropy.Entropy = calcEntropy(tmp);
+            entropy.push_back(curr_entropy);
+
+            outfile << curr_entropy.Entropy << '\t';
+            cout << curr_entropy.Entropy << '\t';
+        }
+        outfile << endl;
+        cout << endl;
+    }
+
+    cout << "CL Size" << ClipLimit.size() << endl;
+
+    outfile.close();
     //**************************************************************************
     /* CURVE FITTING */
+    //**************************************************************************
+
     //**************************************************************************
     /* BREAKPOINT DETECTION */
     //**************************************************************************
@@ -143,33 +227,37 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-/*! @fn float calcBlur (Mat frame)
-    @brief Calculates the "blur" of given \a Mat frame, based on the standard deviation of the Laplacian of the input fram
+/*! @fn float calcEntropy (Mat frame)
+    @brief Calculates the Entropy for the given frame
     @param frame OpenCV matrix container of the input frame
-	@retval The estimated blur for the given frame
+	@retval The estimated entropy for the given frame
 */
-float calcEntropy(Mat image) {
+double calcEntropy(Mat image) {
     // Avg time: 0.7 ms GPU/ 23ms CPU
-    Mat grey, laplacian;
-    cuda::GpuMat gpuFrame, gpuLaplacian;
-    cvtColor(image, grey, COLOR_BGR2GRAY);
+    /*def Entropia(imagen):
+    histograma = cv2.calcHist([imagen],[0],None,[256],[0,256])
+    histograma = histograma.ravel()/histograma.sum()
+    AproxLogs = np.log2(histograma+0.00001)
+    entropia = -1 * (histograma*AproxLogs).sum()
+    return entropia*/
 
-    //upload into GPU memory
-    gpuFrame.upload(grey);
-    //perform Laplacian filter
-    Ptr<cuda::Filter> filter = cuda::createLaplacianFilter(gpuFrame.type(), gpuLaplacian.type(), 1, 1);
-    filter->apply(gpuFrame, gpuLaplacian);    //**/
-    //download from GPU memory
-    gpuLaplacian.download(laplacian);
+    MatND hist;
+    int channels[] = {0};
+    float grange[] = {0.0, 256.0};
+    const float *range[] = {grange};
+    int histSize[] = {256};
+    //computing the histogram for the input image
+    calcHist(&image, 1, channels, Mat(), hist, 1, histSize, range, true, false);
 
-    //we compute the laplacian of the current frame
-    Scalar mean, stdev;
-    // the we compute the mean and stdev of that frame
-    meanStdDev(laplacian, mean, stdev);
-    double m = mean.val[0];
-    double s = stdev.val[0];
-    // we return the standar deviation
-    return s;
+    //we compute the sum of all elements (for a given histogram of a MxN matrix, it should give MxN)
+    double sum = image.size().width * image.size().height;
+
+    hist = hist / sum;
+    Mat aprox_log;
+    cv::log(hist + 0.00001, aprox_log);
+
+    double e = -1.0 * cv::sum((hist.mul(aprox_log)))[0];
+    return e;
 }
 
 
